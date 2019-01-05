201bdb88a312db0c5402a1611995d64992a82fad
A <joy_csharp@yahoo.com>
2019-01-05 19:04:26 +0800

    update

---
+++ Eisk.Core.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Core.Exceptions;
+using System;
+using Xunit;
+
+namespace Eisk.Core.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            CoreException cex = new CoreException();
+        }
+    }
+}
