1bbeba7a4c8ed33635d7fa2649a4ef06e1b312c7
AshrafAlam <joy_csharp@yahoo.com>
2019-01-20 15:41:51 +0800

    u

--- Eisk.Core/DataService/EFCore/EntityContextDataService.cs
+++ Eisk.Core/DataService/EFCore/EntityContextDataService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 
-namespace Eisk.Core.DataService.EntityFrameworkCore
+namespace Eisk.Core.DataService.EFCore
 {
     public class EntityContextDataService<TEntity> : IEntityDataService<TEntity> where TEntity : class, new()
     {
